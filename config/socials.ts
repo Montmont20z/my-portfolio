40706605c5cfc1eb6f942936b2297debcddd9ade
mont <mont@mont>
2026-01-02 18:51:52 +0800

    Feature: Update Footer link

--- config/socials.ts
+++ config/socials.ts
@@ -10,26 +10,26 @@ interface SocialInterface {
 export const SocialLinks: SocialInterface[] = [
   {
     name: "Github",
-    username: "@namanbarkiya",
+    username: "@Montmont20z",
     icon: Icons.gitHub,
-    link: "https://github.com/namanbarkiya",
+    link: "https://github.com/Montmont20z",
   },
   {
     name: "LinkedIn",
     username: "Melvin Cheah",
     icon: Icons.linkedin,
-    link: "https://www.linkedin.com/in/namanbarkiya",
+    link: "https://www.linkedin.com/in/melvin-cheah-3516a0196/",
   },
   {
     name: "Twitter",
-    username: "@namanbarkiya",
+    username: "@Montmont20z",
     icon: Icons.twitter,
-    link: "https://twitter.com/namanbarkiya",
+    link: "https://twitter.com/",
   },
   {
     name: "Gmail",
-    username: "naman.barkiya02",
+    username: "melvincheah20",
     icon: Icons.gmail,
-    link: "mailto:naman.barkiya02@gmail.com",
+    link: "mailto:melvincheah20@gmail.com",
   },
 ];
